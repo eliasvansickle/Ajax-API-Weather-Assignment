caba0b23d38d21a7b7afaf3e274be21bbef8671c
eliasvansickle <eliasvansickle@gmail.com>
2015-06-09 19:57:25 -0700

    recommit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+	$("form").submit(function() {
+		var place = $("input[name = search]").val();
+		$.get("http://api.openweathermap.org/data/2.5/weather?q="+"", function(res), "json");
 
+		return false;
+	});
 });
+
