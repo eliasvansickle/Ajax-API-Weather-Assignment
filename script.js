36042d5e734c433d78a52e9b85d6b4cd3d435e10
eliasvansickle <eliasvansickle@gmail.com>
2015-06-09 20:24:18 -0700

    jQuery Weather app working well moving on to cleanup

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$("form").submit(function() {
 		var place = $("input[name = search]").val();
-		$.get("http://api.openweathermap.org/data/2.5/weather?q="+"", function(res), "json");
-
+		$.get("http://api.openweathermap.org/data/2.5/weather?q="+place+"", function(res) {
+			$(".place").html("<h1>"+place+"</h1>");
+			$(".temp").html("<p>Temperature: "+Math.floor((((res.main.temp - 273)*(9/5)) + 32))+"</p>");
+		}, "json");
 		return false;
 	});
 });
